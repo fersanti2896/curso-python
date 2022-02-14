15a5fd2996b0f67fe07ed61afb56b737018ce75a
Fernando Nicolas <fersanti2896@gmail.com>
2022-02-14 16:38:37 -0600

    Agregando ejercicios Seccion 02-Declaraciones

---
+++ 02-Declaraciones/ciclo-for.py
@@ -0,0 +1,16 @@
+# Ciclo For
+
+for i in range(10):
+    print('Soy:', i)
+
+
+lista = [1, 2, 3, 2, 1, 3, 5]
+
+for v in lista:
+    print('Valor:', v)
+
+for num in range(21):
+    if num % 2 == 0:
+        print(f'Número par: {num}')
+    else:
+        print(f'Numero impar: {num}')
---
+++ 02-Declaraciones/if-else.py
@@ -0,0 +1,10 @@
+
+hambre = False
+sed = False
+
+if hambre and not sed:
+    print('Tengo hambre')
+elif hambre and sed:
+    print('Tengo hambre y sed')
+else:
+    print('No tengo hambre')
